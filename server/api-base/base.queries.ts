e290f4f60aa16f14597f59dfeeac674321b89e90
saTuele <154453237+saTuele@users.noreply.github.com>
2025-08-01 16:36:21 +0200

    initial commit

--- server/api-base/base.queries.ts
+++
@@ -1,6 +0,0 @@
-import { IDetailQuery, IDetailDTO, IListDTO } from "src/api-interfaces/queries.interfaces.js";
-
-export abstract class BaseQueries {
-  public abstract DetailQuery(qry: IDetailQuery): Promise<IDetailDTO>;
-  public abstract ListQuery(): Promise<IListDTO>;
-}
---
+++ server/src/server.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+});
